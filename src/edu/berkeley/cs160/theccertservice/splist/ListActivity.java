bc81e776ef21bb3d41ec538334a643309baf3600
blchang <old.lucky7@gmail.com>
2013-04-07 14:29:49 -0700

    updated and gitignore file fixed

--- src/edu/berkeley/cs160/theccertservice/splist/ListActivity.java
+++ src/edu/berkeley/cs160/theccertservice/splist/ListActivity.java
@@ -2,14 +2,24 @@ package edu.berkeley.cs160.theccertservice.splist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class ListActivity extends Activity {
-
+    private ListView myList;
+//    private MyAdapter myAdapter;
+    
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.list);
+		
+//        myList = (ListView) findViewById(R.id.MyList);
+//        myList.setItemsCanFocus(true);
+//        myAdapter = new MyAdapter();
+//        myList.setAdapter(myAdapter);
 
 	}
+	
+	
 }
