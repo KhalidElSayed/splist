45416723898c518ae7e840a680ad5c60a713d52d
blchang <old.lucky7@gmail.com>
2013-04-07 16:17:05 -0700

    new list dialog

--- src/edu/berkeley/cs160/theccertservice/splist/ListActivity.java
+++ src/edu/berkeley/cs160/theccertservice/splist/ListActivity.java
@@ -1,11 +1,25 @@
 package edu.berkeley.cs160.theccertservice.splist;
 
+import java.util.ArrayList;
 import android.app.Activity;
+import android.app.DialogFragment;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class ListActivity extends Activity {
     private ListView myList;
+    private ArrayList<String> sharedLists = new ArrayList<String>();
+    Spinner currentList;
+    ArrayAdapter<String> arrayAdapter;
 //    private MyAdapter myAdapter;
     
 	/** Called when the activity is first created. */
@@ -14,6 +28,13 @@ public class ListActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.list);
 		
+		if(sharedLists.size() == 0){
+			showCreateListDialog(null);
+		}
+		currentList = (Spinner) findViewById(R.id.lists);
+		arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, sharedLists);
+		currentList.setAdapter(arrayAdapter);
+		currentList.setOnItemSelectedListener(new ChooseListListener());
 //        myList = (ListView) findViewById(R.id.MyList);
 //        myList.setItemsCanFocus(true);
 //        myAdapter = new MyAdapter();
@@ -21,5 +42,60 @@ public class ListActivity extends Activity {
 
 	}
 	
+	public void showCreateListDialog(View v) {
+	    DialogFragment newFragment = new CreateListDialog();
+	    newFragment.show(getFragmentManager(), "createList");
+	}
 	
+	public void onFinishCreateList(String listName){
+		sharedLists.add(listName);
+	}
+	
+	public class CreateListDialog extends DialogFragment {
+		private EditText mEditText;
+
+		public CreateListDialog() {
+			// Empty constructor required for DialogFragment
+		}
+
+		@Override
+		public View onCreateView(LayoutInflater inflater, ViewGroup container,
+				Bundle savedInstanceState) {
+			View view = inflater.inflate(R.layout.create_list, container);
+			mEditText = (EditText) view.findViewById(R.id.list_name);
+			Button create = (Button) view.findViewById(R.id.create_button);
+			Button cancel = (Button) view.findViewById(R.id.cancel_button);
+			create.setOnClickListener(new View.OnClickListener() {
+	            public void onClick(View v) {
+	            	ListActivity parentAct = (ListActivity) getActivity();
+	                parentAct.onFinishCreateList(mEditText.getText().toString());                
+	                done();
+	            }
+	        });
+			cancel.setOnClickListener(new View.OnClickListener() {
+				public void onClick(View v) {
+					done();
+				}
+			});
+			getDialog().setTitle("Create List");
+
+			return view;
+		}
+		
+		public void done() {
+			this.dismiss();
+		}
+	}
+	
+	public class ChooseListListener extends Activity implements OnItemSelectedListener {
+		
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+			
+		}
+		
+		@Override
+		public void onNothingSelected(AdapterView<?> parent) {
+		}
+	}
 }
