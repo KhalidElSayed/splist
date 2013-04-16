4b1cd7c5eb408b18d1c66bb29d9fadf88a3c96e1
jambajoyce <jambajoyce@gmail.com>
2013-04-15 22:08:24 -0700

    Update Item.java
    list of people who are sharing the item

--- src/edu/berkeley/cs160/theccertservice/splist/Item.java
+++ src/edu/berkeley/cs160/theccertservice/splist/Item.java
@@ -5,12 +5,15 @@ public class Item {
 	Boolean _shared;
 	Integer _numPeopleSharing;
 	Double _price;
+	ArrayList<Person> _peopleSharing;
+	ShoppingList _list;
 	
 	public Item(String name) {
 		_name = name;
 		_shared = false;
 		_numPeopleSharing = 0;
 		_price = 0;
+		//ArrayList<Person> _peopleSharing;
 	}
 	
 	public String getName() {
@@ -39,5 +42,9 @@ public class Item {
 	
 	public Double getPrice() {
 		return _price;
+	}
+	
+	public ArrayList<Person> getPeopleSharing() {
+		return _peopleSharing;
 	}
 }
