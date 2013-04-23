242aed6fcda2d517961a63bf38ee482dcd0a6a88
leonguan <htz0426@yahoo.com>
2013-04-23 01:59:39 -0700

    change feed tab and add friends tab

--- src/edu/berkeley/cs160/theccertservice/splist/FriendsActivity.java
+++ src/edu/berkeley/cs160/theccertservice/splist/FriendsActivity.java
@@ -2,14 +2,22 @@ package edu.berkeley.cs160.theccertservice.splist;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ExpandableListView;
 
 public class FriendsActivity extends Activity {
 
+	ExpandableListView exv;
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.friends);
-
+		displayFeeds();
+	}
+	
+	private void displayFeeds(){
+		exv = (ExpandableListView)findViewById(R.id.expandableListView2);
+		exv.setAdapter(new FriendAdapter(this));
+		
 	}
 }
--- src/edu/berkeley/cs160/theccertservice/splist/Person.java
+++ src/edu/berkeley/cs160/theccertservice/splist/Person.java
@@ -5,43 +5,17 @@ import java.util.ArrayList;
 public class Person {
 	
 	String _name;
-	ArrayList<ShoppingList> _myLists;
-	ArrayList<ShoppingList> _listsSharedWithMe;
-	ArrayList<Person> _myFriends;
 	Integer _phoneNumber;
 	String _email;
 	
 	public Person(String name, String email) {
 		_name = name;
-		_myLists = new ArrayList<ShoppingList>();
-		_listsSharedWithMe = new ArrayList<ShoppingList>();
-		_myFriends = new ArrayList<Person>();
 		_phoneNumber = 0;
 		_email = email;
 	}
 	
 	public String getName() {
 		return _name;
-	}
-	
-	public ArrayList<ShoppingList> getMyLists() {
-		return _myLists;
-	}
-	
-	public ArrayList<ShoppingList> getListsSharedWithMe() {
-		return _listsSharedWithMe;
-	}
-	
-	public ArrayList<Person> getFriends() {
-		return _myFriends;
-	}
-	
-	public void addFriend(Person person) {
-		_myFriends.add(person);
-	}
-	
-	public void removeFriend(Person person) {
-		_myFriends.remove(person);
 	}
 	
 	public void setEmail(String email) {
